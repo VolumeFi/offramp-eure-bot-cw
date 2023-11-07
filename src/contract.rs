d94a0821870c50d2ac966b9b4f7af7d8dc94be39
webelf101 <m63974529@gmail.com>
2023-11-08 04:22:20 +0800

    more functions

--- src/contract.rs
+++ src/contract.rs
@@ -2,7 +2,7 @@ use crate::ContractError::Unauthorized;
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+    to_binary, Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Uint256,
 };
 use ethabi::{Address, Contract, Function, Param, ParamType, StateMutability, Token, Uint};
 use std::collections::BTreeMap;
@@ -41,6 +41,16 @@ pub fn execute(
         ExecuteMsg::PutSwap { deposit } => swap(deps, info, deposit),
         ExecuteMsg::SetPaloma {} => set_paloma(deps, info),
         ExecuteMsg::UpdateCompass { new_compass } => update_compass(deps, info, new_compass),
+        ExecuteMsg::UpdateRefundWallet { new_refund_wallet } => {
+            update_refund_wallet(deps, info, new_refund_wallet)
+        }
+        ExecuteMsg::UpdateFee { fee } => update_fee(deps, info, fee),
+        ExecuteMsg::UpdateServiceFeeCollector {
+            new_service_fee_collector,
+        } => update_service_fee_collector(deps, info, new_service_fee_collector),
+        ExecuteMsg::UpdateServiceFee { new_service_fee } => {
+            update_service_fee(deps, info, new_service_fee)
+        }
     }
 }
 
@@ -196,6 +206,195 @@ fn update_compass(
             ),
         }))
         .add_attribute("action", "update_compass"))
+}
+
+fn update_refund_wallet(
+    deps: DepsMut,
+    info: MessageInfo,
+    new_refund_wallet: String,
+) -> Result<Response<PalomaMsg>, ContractError> {
+    let state = STATE.load(deps.storage)?;
+    if state.owner != info.sender {
+        return Err(Unauthorized {});
+    }
+    let new_refund_wallet_address: Address = Address::from_str(new_refund_wallet.as_str()).unwrap();
+    #[allow(deprecated)]
+    let contract: Contract = Contract {
+        constructor: None,
+        functions: BTreeMap::from_iter(vec![(
+            "update_refund_wallet".to_string(),
+            vec![Function {
+                name: "update_refund_wallet".to_string(),
+                inputs: vec![Param {
+                    name: "new_refund_wallet".to_string(),
+                    kind: ParamType::Address,
+                    internal_type: None,
+                }],
+                outputs: Vec::new(),
+                constant: None,
+                state_mutability: StateMutability::NonPayable,
+            }],
+        )]),
+        events: BTreeMap::new(),
+        errors: BTreeMap::new(),
+        receive: false,
+        fallback: false,
+    };
+
+    Ok(Response::new()
+        .add_message(CosmosMsg::Custom(PalomaMsg {
+            job_id: state.job_id,
+            payload: Binary(
+                contract
+                    .function("update_refund_wallet")
+                    .unwrap()
+                    .encode_input(&[Token::Address(new_refund_wallet_address)])
+                    .unwrap(),
+            ),
+        }))
+        .add_attribute("action", "update_refund_wallet"))
+}
+
+fn update_fee(
+    deps: DepsMut,
+    info: MessageInfo,
+    fee: Uint256,
+) -> Result<Response<PalomaMsg>, ContractError> {
+    let state = STATE.load(deps.storage)?;
+    if state.owner != info.sender {
+        return Err(Unauthorized {});
+    }
+    #[allow(deprecated)]
+    let contract: Contract = Contract {
+        constructor: None,
+        functions: BTreeMap::from_iter(vec![(
+            "update_fee".to_string(),
+            vec![Function {
+                name: "update_fee".to_string(),
+                inputs: vec![Param {
+                    name: "new_fee".to_string(),
+                    kind: ParamType::Uint(256),
+                    internal_type: None,
+                }],
+                outputs: Vec::new(),
+                constant: None,
+                state_mutability: StateMutability::NonPayable,
+            }],
+        )]),
+        events: BTreeMap::new(),
+        errors: BTreeMap::new(),
+        receive: false,
+        fallback: false,
+    };
+
+    Ok(Response::new()
+        .add_message(CosmosMsg::Custom(PalomaMsg {
+            job_id: state.job_id,
+            payload: Binary(
+                contract
+                    .function("update_fee")
+                    .unwrap()
+                    .encode_input(&[Token::Uint(Uint::from_big_endian(&fee.to_be_bytes()))])
+                    .unwrap(),
+            ),
+        }))
+        .add_attribute("action", "update_fee"))
+}
+
+fn update_service_fee_collector(
+    deps: DepsMut,
+    info: MessageInfo,
+    new_service_fee_collector: String,
+) -> Result<Response<PalomaMsg>, ContractError> {
+    let state = STATE.load(deps.storage)?;
+    if state.owner != info.sender {
+        return Err(Unauthorized {});
+    }
+    let new_service_fee_collector_address: Address =
+        Address::from_str(new_service_fee_collector.as_str()).unwrap();
+    #[allow(deprecated)]
+    let contract: Contract = Contract {
+        constructor: None,
+        functions: BTreeMap::from_iter(vec![(
+            "update_service_fee_collector".to_string(),
+            vec![Function {
+                name: "update_service_fee_collector".to_string(),
+                inputs: vec![Param {
+                    name: "new_service_fee_collector".to_string(),
+                    kind: ParamType::Address,
+                    internal_type: None,
+                }],
+                outputs: Vec::new(),
+                constant: None,
+                state_mutability: StateMutability::NonPayable,
+            }],
+        )]),
+        events: BTreeMap::new(),
+        errors: BTreeMap::new(),
+        receive: false,
+        fallback: false,
+    };
+
+    Ok(Response::new()
+        .add_message(CosmosMsg::Custom(PalomaMsg {
+            job_id: state.job_id,
+            payload: Binary(
+                contract
+                    .function("update_service_fee_collector")
+                    .unwrap()
+                    .encode_input(&[Token::Address(new_service_fee_collector_address)])
+                    .unwrap(),
+            ),
+        }))
+        .add_attribute("action", "update_service_fee_collector"))
+}
+
+fn update_service_fee(
+    deps: DepsMut,
+    info: MessageInfo,
+    new_service_fee: Uint256,
+) -> Result<Response<PalomaMsg>, ContractError> {
+    let state = STATE.load(deps.storage)?;
+    if state.owner != info.sender {
+        return Err(Unauthorized {});
+    }
+    #[allow(deprecated)]
+    let contract: Contract = Contract {
+        constructor: None,
+        functions: BTreeMap::from_iter(vec![(
+            "update_service_fee".to_string(),
+            vec![Function {
+                name: "update_service_fee".to_string(),
+                inputs: vec![Param {
+                    name: "new_service_fee".to_string(),
+                    kind: ParamType::Uint(256),
+                    internal_type: None,
+                }],
+                outputs: Vec::new(),
+                constant: None,
+                state_mutability: StateMutability::NonPayable,
+            }],
+        )]),
+        events: BTreeMap::new(),
+        errors: BTreeMap::new(),
+        receive: false,
+        fallback: false,
+    };
+
+    Ok(Response::new()
+        .add_message(CosmosMsg::Custom(PalomaMsg {
+            job_id: state.job_id,
+            payload: Binary(
+                contract
+                    .function("update_service_fee")
+                    .unwrap()
+                    .encode_input(&[Token::Uint(Uint::from_big_endian(
+                        &new_service_fee.to_be_bytes(),
+                    ))])
+                    .unwrap(),
+            ),
+        }))
+        .add_attribute("action", "update_service_fee"))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
